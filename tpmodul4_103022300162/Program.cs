f0afae2710f02e8780481806281c677d41652052
hafidzmusyafa <165547809+hafidzmusyafa@users.noreply.github.com>
2025-03-10 22:22:06 +0700

    menambahkan implementasi dengan state-based

--- tpmodul4_103022300162/Program.cs
+++ tpmodul4_103022300162/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+public class KodePos
+{
+    public string Kode { get; set; }
+    public string Kelurahan { get; set; }
+}
+public class Doormachine
+{
+    public enum State { terbuka, tekunci }
+
+    private State stateAwal;
+
+    public void Open()
+    {
+        stateAwal = State.terbuka;
+        Console.WriteLine("Pintu terbuka");
+    }
+    public void Close()
+    {
+        stateAwal = State.tekunci;
+        Console.WriteLine("Pintu tertutup");
+    }
+    public void Lock()
+    {
+        stateAwal = State.tekunci;
+        Console.WriteLine("Pintu terkunci");
+    }
+
+    class MainClass
+    {
+        static void Main()
+        {
+            List<KodePos> isiKodePos = new List<KodePos>
+                {
+                    new KodePos { Kelurahan = "Batununggal", Kode = "40266" },
+                    new KodePos { Kelurahan = "Kujangsari", Kode = "40287" },
+                    new KodePos { Kelurahan = "Mengger", Kode = "40267" },
+                    new KodePos { Kelurahan = "Wates", Kode = "40256" },
+                    new KodePos { Kelurahan = "Cijaura", Kode = "40287" },
+                    new KodePos { Kelurahan = "Jatisari", Kode = "40286" },
+                    new KodePos { Kelurahan = "Margasari", Kode = "40286" },
+                    new KodePos { Kelurahan = "Sekejati", Kode = "40286" },
+                    new KodePos { Kelurahan = "Kebonwaru", Kode = "40272" },
+                    new KodePos { Kelurahan = "Maleer", Kode = "40274" },
+                    new KodePos { Kelurahan = "Samoja", Kode = "40273" }
+                };
+
+            foreach (var data in isiKodePos)
+            {
+                Console.WriteLine("Kelurahan: " + data.Kelurahan, "Kode Pos: " + data.Kode);
+            }
+
+            Doormachine pintu = new Doormachine();
+            pintu.Open();
+            pintu.Close();
+            pintu.Lock();
+            pintu.Open();
+        }
+    }
+}
--- tpmodul4_103022300162/obj/Debug/net8.0/tpmodul4_103022300162.AssemblyInfo.cs
+++ tpmodul4_103022300162/obj/Debug/net8.0/tpmodul4_103022300162.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul4_103022300162")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfba3955c70d527bafcb5bee64654d0b0b79a35e")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul4_103022300162")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul4_103022300162")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
